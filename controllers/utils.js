e9b49b701c3dda9ca97a542244c93ecc8c6cae47
Hoang Do <hoangdo.dh94@gmail.com>
2016-03-30 12:59:07 +0700

    update

--- controllers/utils.js
+++ controllers/utils.js
@@ -65,7 +65,7 @@ var refineData = function(data) {
 // Middlewares
 var cors = function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
 };
 
