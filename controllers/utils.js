c4d2c7aa3d7df4502bd471cab84bc46bb86176a2
Hoang Do <hoangdo.dh94@gmail.com>
2016-03-31 15:24:12 +0700

    add Access-Control-Allow-Methods header

--- controllers/utils.js
+++ controllers/utils.js
@@ -66,6 +66,7 @@ var refineData = function(data) {
 var cors = function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
     next();
 };
 
