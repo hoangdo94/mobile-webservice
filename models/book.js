d5e43261c5fe07d16eb8fea9578e7df3a99ff50e
Hoang Do <hoangdo.dh94@gmail.com>
2016-05-12 09:38:44 +0700

    update weights for Books full-text search

--- models/book.js
+++ models/book.js
@@ -3,20 +3,49 @@ var Schema = mongoose.Schema;
 var mongoosePaginate = require('mongoose-paginate');
 
 var bookSchema = new Schema({
-    userId: { type: String, required: true },
-    title: { type: String, required: true },
-    author: { type: String, required: true },
-    publishYear: { type: Number },
-    genres: { type: [String] },
-    review: { type: String, required: true },
-    cover: { type: String, default: 'default.png' },
+    userId: {
+        type: String,
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    },
+    publishYear: {
+        type: Number
+    },
+    genres: {
+        type: [String]
+    },
+    review: {
+        type: String,
+        required: true
+    },
+    cover: {
+        type: String,
+        default: 'default.png'
+    },
     createdAt: Date,
     updatedAt: Date
 });
 
-bookSchema.index({'$**': 'text'});
+bookSchema.index({
+    '$**': 'text'
+}, {
+    'weights': {
+        title: 3,
+        author: 2,
+        publishYear: 2,
+        review: 1,
+        genres: 1
+    }
+});
 
-bookSchema.pre('save', function (next) {
+bookSchema.pre('save', function(next) {
     var now = Date.now();
     if (!this.createdAt) this.createdAt = now;
     this.updatedAt = now;
