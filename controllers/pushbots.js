132aaed48d2dbe8df2c4d02d91ad8f4e24facd20
Hoang Do <hoangdo.dh94@gmail.com>
2016-05-13 23:13:05 +0700

    add pushbots credentials

--- controllers/pushbots.js
+++ controllers/pushbots.js
@@ -2,8 +2,8 @@ var pushbots = require('pushbots');
 
 function sendPushNotification(userId, data) {
   var Pushbots = new pushbots.api({
-    id: '',
-    secret: ''
+    id: '5735edbd4a9efa91a18b4567',
+    secret: 'd22ee26a74cf0e4e2290ef9ecae502f6'
   });
 
   if (data.message) {
